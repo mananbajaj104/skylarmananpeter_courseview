73b8fb46a799e82abc121a13eaae502b758594fd
skyneu <sfn1@rice.edu>
2019-10-26 12:57:29 -0500

    added components, nothing useful in them yet

---
+++ src/components/Graphs.js
@@ -0,0 +1,13 @@
+﻿import React from 'react'
+import Toggle from './components/Toggle'
+
+const Banner = ({className}) => {
+    return (
+        <div>}>
+            <h1>{className}</h1>
+             <Toggle />
+        </div>
+    )
+}
+
+export default Banner;
